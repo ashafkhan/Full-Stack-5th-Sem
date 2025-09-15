574709680a83217f290f0f97110def4c7774b9f4
Mohammad Ashaf Khan <161150645+ashafkhan@users.noreply.github.com>
2025-09-15 22:13:36 +0530

    Update script.js

--- JS-Basics/JS_practice_01/script.js
+++ JS-Basics/JS_practice_01/script.js
@@ -1,1 +1,14 @@
+const textarea = document.getElementById("textInput");
+const counter = document.getElementById("charCounter");
+const maxLength = textarea.getAttribute("maxlength");
 
+textarea.addEventListener("input", () => {
+  const length = textarea.value.length;
+  counter.textContent = `${length} / ${maxLength}`;
+
+  if (length >= maxLength) {
+    counter.classList.add("exceeded");
+  } else {
+    counter.classList.remove("exceeded");
+  }
+});
